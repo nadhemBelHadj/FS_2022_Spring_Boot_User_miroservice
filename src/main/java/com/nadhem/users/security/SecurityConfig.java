5d0de52b9d7e856603c2540f49d58f650452d0e7
unknown <nadhemb@yahoo.com>
2023-01-24 16:17:35 +0100

    first commit

--- src/main/java/com/nadhem/users/security/SecurityConfig.java
+++ src/main/java/com/nadhem/users/security/SecurityConfig.java
@@ -1,45 +1,59 @@
 package com.nadhem.users.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig  {
  
- 	@Autowired
+	@Autowired
  	UserDetailsService userDetailsService;
  	
  	@Autowired
 	BCryptPasswordEncoder bCryptPasswordEncoder;
+ 	
+ 	@Autowired
+ 	AuthenticationManager authMgr;
 	
 	
-	@Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-	   auth.userDetailsService(userDetailsService)
-		.passwordEncoder(bCryptPasswordEncoder);
+ 	@Bean
+	public AuthenticationManager authManager(HttpSecurity http, 
+			BCryptPasswordEncoder bCryptPasswordEncoder, 
+			UserDetailsService userDetailsService) 
+	  throws Exception {
+	    return http.getSharedObject(AuthenticationManagerBuilder.class)
+	      .userDetailsService(userDetailsService)
+	      .passwordEncoder(bCryptPasswordEncoder)
+	      .and()
+	      .build();
+	}
 	
-    }
-  
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-	  http.csrf().disable();
-	  http.sessionManagement().
-	  			sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-	  http.authorizeRequests().antMatchers("/login").permitAll();  
-	  http.authorizeRequests().antMatchers("/all").hasAuthority("ADMIN");
-
-	  http.authorizeRequests().anyRequest().authenticated();
-	  http.addFilter(new  JWTAuthenticationFilter (authenticationManager())) ;
-	  http.addFilterBefore(new JWTAuthorizationFilter(),UsernamePasswordAuthenticationFilter.class);
-
+	
+	 @Bean
+     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		 
-  }
+		  http.csrf().disable();
+		  http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		  http.authorizeRequests().antMatchers("/login").permitAll();  
+		  http.authorizeRequests().antMatchers("/all").hasAuthority("ADMIN");
+		  http.authorizeRequests().anyRequest().authenticated();
+		  
+		  http.addFilter(new  JWTAuthenticationFilter (authMgr)) ;
+		  http.addFilterBefore(new JWTAuthorizationFilter(),UsernamePasswordAuthenticationFilter.class);
+		 		
+		  return http.build();
+   
+     }
+
 
 }
